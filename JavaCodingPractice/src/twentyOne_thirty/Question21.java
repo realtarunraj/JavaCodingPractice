102c45b1bbe1f11302aae7298f9b338398a2dcee
Tarun Kumar Raj <rajtarunkumar007@gmail.com>
2023-10-18 17:53:52 +0530

    Counting word seperated by comma(,), tab(\t) or line change(\n)

--- JavaCodingPractice/src/twentyOne_thirty/Question21.java
+++ JavaCodingPractice/src/twentyOne_thirty/Question21.java
@@ -1,103 +1,18 @@
 package twentyOne_thirty;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
-class Employee implements Comparable<Employee> {
-
-	private int id;
-	private String name;
-	private Double salary;
-
-	public Employee(int id, String name, Double salary) {
-		this.id = id;
-		this.name = name;
-		this.salary = salary;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Double getSalary() {
-		return salary;
-	}
-
-	public void setSalary(Double salary) {
-		this.salary = salary;
-	}
-
-	@Override
-	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
-	}
-
-	// Overriding by Comparable
-
-	@Override
-	public int compareTo(Employee other) {
-		return Integer.compare(this.id, other.id);
-	}
-
-	// Comparator use ......
-
-	public static Comparator<Employee> nameSorting = new Comparator<Employee>() {
-
-		@Override
-		public int compare(Employee o1, Employee o2) {
-			return o1.getName().compareTo(o2.getName());
-		}
-	};
-
-	public static Comparator<Employee> salarySorting = new Comparator<Employee>() {
-
-		@Override
-		public int compare(Employee o1, Employee o2) {
-			return o1.getSalary().compareTo(o2.getSalary());
-		}
-	};
-
-}
-
-public class Question21 {
+public class Question21{
 	public static void main(String[] args) {
-
-		List<Employee> employee = new ArrayList<>();
-		employee.add(new Employee(14, "Tarun", 90000.0));
-		employee.add(new Employee(7, "Mugdha", 80000.0));
-		employee.add(new Employee(62, "Deepak", 60000.0));
-		employee.add(new Employee(34, "Shreyanka", 40000.0));
-
-		// Using Comparable
-		Collections.sort(employee);
-		for (Employee emp : employee) {
-			System.out.println(emp);
-		}
-
-		// Name Sorting Using Comparator
-		Collections.sort(employee, Employee.nameSorting);
-		for (Employee emp : employee) {
-			System.out.println(emp);
-		}
-
-		// Salary Sorting using Comparator
-		Collections.sort(employee, Employee.salarySorting);
-		for (Employee emp : employee) {
-			System.out.println(emp);
-		}
+		
+		String bySpace = "My name is Tarun Kumar Raj";
+		String byTab = "I	am	Tarun	Raj";
+		String lineChange = "my name \n is tarun \n raj";
+		
+		String[] stringBySpace = bySpace.split(" ");
+		String[] stringByTab = byTab.split("\t");
+		String[] stringLineChange = lineChange.split("\n");
+		
+		System.out.println(stringBySpace.length); // Output = 6
+		System.out.println(stringByTab.length); //Output = 4
+		System.out.println(stringLineChange.length); // Output = 3
 	}
 }
