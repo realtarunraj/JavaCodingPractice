c1b4a966f3309279cf16329d51111c31f9553a91
Tarun Kumar Raj <rajtarunkumar007@gmail.com>
2023-10-19 19:22:23 +0530

    Prime number upto some number question

--- JavaCodingPractice/src/twentyOne_thirty/Question23.java
+++ JavaCodingPractice/src/twentyOne_thirty/Question23.java
@@ -1,5 +1,40 @@
 package twentyOne_thirty;
 
-public class Question23 {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
+public class Question23 {
+	public static List<Integer> primeNumber(int num){
+		List<Integer> primeList = new ArrayList<>();
+		
+		while(num > 1) {
+			int count = 0;
+			for(int i=2; i<num; i++) {
+				if(num%i == 0) {
+					count++;
+				}
+			}
+			
+			if(count == 0) {
+				primeList.add(num);
+			}
+			
+			num--;
+		}
+		
+		Collections.reverse(primeList);
+		return primeList;
+	}
+	
+	public static void main(String[] args) {
+		System.out.print("Enter the number till which You want prime numbers :- ");
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		sc.close();
+		
+		System.out.println(primeNumber(num));
+	}
 }
