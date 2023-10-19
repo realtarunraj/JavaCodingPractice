68d78d97d6882cf3bfeb9e731490f686ff86278a
Tarun Kumar Raj <rajtarunkumar007@gmail.com>
2023-10-19 16:45:35 +0530

    Added Palindrome Problem

--- JavaCodingPractice/src/twentyOne_thirty/Question22.java
+++ JavaCodingPractice/src/twentyOne_thirty/Question22.java
@@ -1,5 +1,28 @@
 package twentyOne_thirty;
 
-public class Question22 {
+import java.util.Scanner;
 
+public class Question22 {
+	public static void main(String[] args) {
+		System.out.print("write a number :- ");
+		Scanner sc = new Scanner(System.in);
+		long num = sc.nextLong();
+
+		long num1 = num;
+
+		long check = 0;
+		do {
+			check = (check * 10) + (num % 10);
+			num = num / 10;
+		} while (num > 0);
+		
+		
+		if (check == num1) {
+			System.out.println("Palindrome");
+		} else {
+			System.out.println("Not Palindrome");
+		}
+
+		sc.close();
+	}
 }
