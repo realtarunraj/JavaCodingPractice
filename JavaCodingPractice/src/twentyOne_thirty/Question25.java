4cd4d2b288e6f6624c33e078e8d1a2e4596015ef
Tarun Kumar Raj <rajtarunkumar007@gmail.com>
2023-10-19 20:19:02 +0530

    magic Number Coding

--- JavaCodingPractice/src/twentyOne_thirty/Question25.java
+++ JavaCodingPractice/src/twentyOne_thirty/Question25.java
@@ -1,5 +1,33 @@
 package twentyOne_thirty;
 
+import java.util.Scanner;
+
 public class Question25 {
+	
+	public static String magicNumber(int num) {
+		int sum = 0;
+		while(num > 0) {
+			sum = sum + num%10;
+			num = num/10;
+		}
+		
+		if(sum <10) {
+			if(sum == 1) {
+				return "Magic Number";
+			}
+			else {
+				return "It's not a Magic Number.";
+			}
+		}
+		
+		return magicNumber(sum);
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		System.out.println(magicNumber(num));
+	}
 
 }
