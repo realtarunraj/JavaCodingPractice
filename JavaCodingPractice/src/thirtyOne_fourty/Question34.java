f6a96d38e2ffd27763c4a0c19133e02d3cf3e847
Tarun Kumar Raj <rajtarunkumar007@gmail.com>
2023-10-29 00:43:03 +0530

    getting Non repeated character

--- JavaCodingPractice/src/thirtyOne_fourty/Question34.java
+++ JavaCodingPractice/src/thirtyOne_fourty/Question34.java
@@ -1,5 +1,22 @@
 package thirtyOne_fourty;
 
-public class Question34 {
+import java.util.Scanner;
 
+public class Question34 {
+	public static void main(String[] args) {
+
+		System.out.print("Write any word : ");
+
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		
+		System.out.println(
+				str.chars()
+				.mapToObj(x -> String.valueOf((char)x))
+				.filter(x -> str.indexOf(x) == str.lastIndexOf(x))
+				.toList()
+				);
+		
+		sc.close();
+	}
 }
