19895aceeb28f808407ce25b2ad2a048375eae60
Tarun Kumar Raj <rajtarunkumar007@gmail.com>
2023-10-30 20:38:25 +0530

    Check for Anagram

--- JavaCodingPractice/src/thirtyOne_fourty/Question35.java
+++ JavaCodingPractice/src/thirtyOne_fourty/Question35.java
@@ -1,5 +1,19 @@
 package thirtyOne_fourty;
 
-public class Question35 {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Question35 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String str1 = sc.next();
+		String str2 = sc.next();
+
+		boolean result = Arrays.equals(str1.chars().sorted().toArray(), str2.chars().sorted().toArray());
+
+		System.out.println(result);
+		
+		sc.close();
+	}
 }
