8b36f3454c311c99db8a786260aeb88fd2aef2ea
Tarun Kumar Raj <rajtarunkumar007@gmail.com>
2023-10-31 23:29:19 +0530

    First non repeated character in string

--- JavaCodingPractice/src/thirtyOne_fourty/Question37.java
+++ JavaCodingPractice/src/thirtyOne_fourty/Question37.java
@@ -1,5 +1,18 @@
 package thirtyOne_fourty;
 
-public class Question37 {
+import java.util.List;
+import java.util.Scanner;
 
+public class Question37 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+
+		List<String> list = str.chars().mapToObj(x -> String.valueOf((char) x))
+				.filter(x -> str.indexOf(x) == str.lastIndexOf(x)).toList();
+
+		System.out.println("First non repeted character is : " + list.get(0));
+
+		sc.close();
+	}
 }
