5c9751d4408f18970aab82819e74e29d58a10df8
Tarun Kumar Raj <rajtarunkumar007@gmail.com>
2023-11-03 13:18:48 +0530

    Missing Items

--- JavaCodingPractice/src/thirtyOne_fourty/Question38.java
+++ JavaCodingPractice/src/thirtyOne_fourty/Question38.java
@@ -5,12 +5,30 @@ import java.util.stream.IntStream;
 
 public class Question38 {
 	public static void main(String[] args) {
-		
+
 		int[] oneToHundred = IntStream.rangeClosed(1, 100).toArray();
-		
+
 		// deleting 72 and 84
 		int[] finalArray = Arrays.stream(oneToHundred).filter(x -> x != 72 && x != 84).toArray();
+		for (int i : finalArray) {
+			System.out.print(i + " ");
+		}
 		
+		System.out.print("\nMissing Items : ");
 		
+		int count = 1;
+		int i = 0;
+		
+		while (i < finalArray.length) {
+			if (count == finalArray[i]) {
+				i++;
+				count++;
+				continue;
+			}
+			else {
+				System.out.print(count + " ");
+				count++;
+			}
+		}
 	}
 }
