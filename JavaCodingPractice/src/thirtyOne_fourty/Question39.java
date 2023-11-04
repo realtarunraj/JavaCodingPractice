5ebdad5feaf006127a266d86ca6f75a9139b43dc
Tarun Kumar Raj <rajtarunkumar007@gmail.com>
2023-11-04 14:59:21 +0530

    largest and smallest elements of an array

---
+++ JavaCodingPractice/src/thirtyOne_fourty/Question39.java
@@ -0,0 +1,17 @@
+package thirtyOne_fourty;
+
+import java.util.Scanner;
+
+public class Question39 {
+	public static void main(String[] args) {
+		System.out.print("Enter the inputs seperated by Spaces : ");
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		
+		String[] inputArray = str.split(" ");
+		
+		System.out.println(inputArray);
+	}
+}
+
+// 8252685359
