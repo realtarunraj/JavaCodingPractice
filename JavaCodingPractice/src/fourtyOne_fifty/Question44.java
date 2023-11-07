6fe4d661cfbf1945dfd35f553b1ca80e6dd04657
Tarun Kumar Raj <rajtarunkumar007@gmail.com>
2023-11-07 17:22:20 +0530

    Counting characters in word

--- JavaCodingPractice/src/fourtyOne_fifty/Question44.java
+++ JavaCodingPractice/src/fourtyOne_fifty/Question44.java
@@ -1,7 +1,16 @@
 package fourtyOne_fifty;
 
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class Question44 {
 	public static void main(String[] args) {
+		String str = "crosssubsidize";
+
+		Map<Character, Long> map = str.chars().mapToObj(x -> (char) x)
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 		
+		map.forEach((c, i) -> System.out.println(c + " : " + i));
 	}
 }
