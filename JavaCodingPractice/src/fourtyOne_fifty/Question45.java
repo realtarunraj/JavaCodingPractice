e34045659a47a78eb418036763ede792ba90f724
Tarun Kumar Raj <rajtarunkumar007@gmail.com>
2023-11-07 20:13:38 +0530

    Factor of any number

--- JavaCodingPractice/src/fourtyOne_fifty/Question45.java
+++ JavaCodingPractice/src/fourtyOne_fifty/Question45.java
@@ -1,5 +1,18 @@
 package fourtyOne_fifty;
 
-public class Question45 {
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+public class Question45 {
+	public static void main(String[] args) {
+		// Find out factors of given numbers. n=34
+		
+		int num = 34;
+		
+		System.out.println(
+				
+				IntStream.rangeClosed(1, num).filter(x -> num%x == 0).boxed().collect(Collectors.toList())
+				
+				);
+	}
 }
