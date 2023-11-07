eb28e49a1dcf11fbc0c1d55206df8d7619f4f6ec
Tarun Kumar Raj <rajtarunkumar007@gmail.com>
2023-11-07 11:14:13 +0530

    remove special character in a string

--- JavaCodingPractice/src/fourtyOne_fifty/Question42.java
+++ JavaCodingPractice/src/fourtyOne_fifty/Question42.java
@@ -1,8 +1,20 @@
 package fourtyOne_fifty;
 
+import java.util.stream.Collectors;
+
 public class Question42 {
 	// Range for lower case 97-122
 	public static void main(String[] args) {
-		
+		String str = "fret%$gtu(hdg&sgdfdf$";
+
+		System.out.println(
+
+				str.chars() // Stream
+				.filter(x -> x > 97 && x < 122)
+				.mapToObj(x -> String.valueOf((char) (x)))
+				.distinct()
+				.collect(Collectors.toList())
+
+		);
 	}
 }
