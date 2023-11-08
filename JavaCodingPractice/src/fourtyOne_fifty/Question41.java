62a61be7ba9fb5a503e97844583ae8e7d65992c9
Tarun Kumar Raj <rajtarunkumar007@gmail.com>
2023-11-08 15:56:09 +0530

    made some modifications

--- JavaCodingPractice/src/fourtyOne_fifty/Question41.java
+++ JavaCodingPractice/src/fourtyOne_fifty/Question41.java
@@ -3,6 +3,6 @@ package fourtyOne_fifty;
 // Merge Sorting
 public class Question41 {
 	public static void main(String[] args) {
-
+		
 	}
 }
--- JavaCodingPractice/src/fourtyOne_fifty/Question48.java
+++ JavaCodingPractice/src/fourtyOne_fifty/Question48.java
@@ -1,5 +1,7 @@
 package fourtyOne_fifty;
 
 public class Question48 {
+	public static void main(String[] args) {
 
+	}
 }
