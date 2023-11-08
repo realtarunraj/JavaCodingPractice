9ff391e1a23f9ead74b2b07568449ea2ffa78d8e
Tarun Kumar Raj <rajtarunkumar007@gmail.com>
2023-11-08 15:54:37 +0530

    Getting duplicate characters

--- JavaCodingPractice/src/fourtyOne_fifty/Question47.java
+++ JavaCodingPractice/src/fourtyOne_fifty/Question47.java
@@ -1,5 +1,16 @@
 package fourtyOne_fifty;
 
-public class Question47 {
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class Question47 {
+	public static void main(String[] args) {
+		List<Character> list = Arrays.asList('a','b','a','c', 'c', 'a','b','a','c', 'c', 'd', 'm', 'n', 'm');
+		
+		System.out.print("Character which are duplicate in the entry are : ");
+		List<Character> result = list.stream().filter(x -> list.indexOf(x) != list.lastIndexOf(x)).distinct().collect(Collectors.toList());
+		
+		result.forEach(x -> System.out.print(x + " "));
+	}
 }
