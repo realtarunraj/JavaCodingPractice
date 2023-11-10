c05ed7ed98593e5da5b9c6a19abbbf52826db7a6
Tarun Kumar Raj <rajtarunkumar007@gmail.com>
2023-11-10 17:18:03 +0530

    modification

--- JavaCodingPractice/src/fiftyone_sixtyone/Question51.java
+++ JavaCodingPractice/src/fiftyone_sixtyone/Question51.java
@@ -1,8 +1,23 @@
 package fiftyone_sixtyone;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
 public class Question51 {
 	public static void main(String[] args) {
-		
-		
+
+		List<String> list = Arrays.asList("aman", "tarun", "aman", "raj", "aman", "deepak", "aman", "tarun");
+		Map<String, Integer> map = new HashMap<>();
+
+		for (String str : list) {
+			if (map.containsKey(str)) {
+				map.replace(str, map.get(str) + 1);
+			} else {
+				map.put(str, 1);
+			}
+		}
 	}
 }
--- JavaCodingPractice/src/fourtyOne_fifty/Question47.java
+++ JavaCodingPractice/src/fourtyOne_fifty/Question47.java
@@ -6,11 +6,12 @@ import java.util.stream.Collectors;
 
 public class Question47 {
 	public static void main(String[] args) {
-		List<Character> list = Arrays.asList('a','b','a','c', 'c', 'a','b','a','c', 'c', 'd', 'm', 'n', 'm');
-		
+		List<Character> list = Arrays.asList('a', 'b', 'a', 'c', 'c', 'a', 'b', 'a', 'c', 'c', 'd', 'm', 'n', 'm');
+
 		System.out.print("Character which are duplicate in the entry are : ");
-		List<Character> result = list.stream().filter(x -> list.indexOf(x) != list.lastIndexOf(x)).distinct().collect(Collectors.toList());
-		
+		List<Character> result = list.stream().filter(x -> list.indexOf(x) != list.lastIndexOf(x)).distinct()
+				.collect(Collectors.toList());
+
 		result.forEach(x -> System.out.print(x + " "));
 	}
 }
