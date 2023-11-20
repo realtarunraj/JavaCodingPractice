e6c4f6027a738e6429688b9a3ae8e742eac39567
Tarun Kumar Raj <rajtarunkumar007@gmail.com>
2023-11-21 00:36:03 +0530

    character at specific index

--- JavaCodingPractice/src/fiftyone_sixtyone/Question51.java
+++ JavaCodingPractice/src/fiftyone_sixtyone/Question51.java
@@ -1,7 +1,21 @@
 package fiftyone_sixtyone;
 
+import java.util.Scanner;
+
 public class Question51 {
 	public static void main(String[] args) {
+		String str = "Capgemini";
 
+		System.out.print("Enter the index : ");
+		Scanner sc = new Scanner(System.in);
+		int index = sc.nextInt();
+
+		if (index >= 0 && index < str.length()) {
+			System.out.println("The character at passed index is : " + str.toLowerCase().charAt(index));
+		} else {
+			System.out.println("Passed index is invalid.");
+		}
+
+		sc.close();
 	}
 }
