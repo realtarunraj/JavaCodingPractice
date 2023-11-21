acf6804f3c0598c313bdc7602bf3df13870065b6
Tarun Kumar Raj <rajtarunkumar007@gmail.com>
2023-11-21 18:04:57 +0530

    modification

--- JavaCodingPractice/src/fiftyone_sixtyone/Question52.java
+++ JavaCodingPractice/src/fiftyone_sixtyone/Question52.java
@@ -1,7 +1,14 @@
 package fiftyone_sixtyone;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Question52 {
 	public static void main(String[] args) {
-
+		String str1 = "fireman";
+		String str2 = "fire";
+		
+		List<Character> list1 = str1.chars().mapToObj(x -> (char)(x)).collect(Collectors.toList());
+		List<Character> list2 = str2.chars().mapToObj(x -> (char)(x)).collect(Collectors.toList());
 	}
 }
