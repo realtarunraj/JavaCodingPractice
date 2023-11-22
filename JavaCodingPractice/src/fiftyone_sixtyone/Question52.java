3d0b1c07677640b0d9c43044fdaa80f6e5033ba9
Tarun Kumar Raj <rajtarunkumar007@gmail.com>
2023-11-22 09:03:23 +0530

    check character of one string present in another string

--- JavaCodingPractice/src/fiftyone_sixtyone/Question52.java
+++ JavaCodingPractice/src/fiftyone_sixtyone/Question52.java
@@ -10,5 +10,9 @@ public class Question52 {
 		
 		List<Character> list1 = str1.chars().mapToObj(x -> (char)(x)).collect(Collectors.toList());
 		List<Character> list2 = str2.chars().mapToObj(x -> (char)(x)).collect(Collectors.toList());
+		
+		boolean check = list2.stream().allMatch(list1::contains);
+		
+		System.out.println("Is the character in list2 is in list1 ?? \n" + check);
 	}
 }
