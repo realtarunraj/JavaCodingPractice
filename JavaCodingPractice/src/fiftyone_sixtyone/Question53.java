6cc48f5675fffc528a1308bca238435a1743bef6
Tarun Kumar Raj <rajtarunkumar007@gmail.com>
2023-11-28 10:42:36 +0530

    Primr Number Sum

--- JavaCodingPractice/src/fiftyone_sixtyone/Question53.java
+++ JavaCodingPractice/src/fiftyone_sixtyone/Question53.java
@@ -1,10 +1,23 @@
 package fiftyone_sixtyone;
 
 public class Question53 {
-	
-	
-	
 	public static void main(String[] args) {
+		long arr[] = new long[1000 + 1];
 		
+		for (int i = 2; i <= 1000; i++) {
+			arr[i] = 0;
+		}
+		
+		for (int j = 2; j <= 1000; j++) {
+			if (arr[j] == 0) {
+				for (int k = j * 2; k <= 1000; k += j) {
+					arr[k] = 1;
+				}
+			}
+		}
+		
+		for(long l : arr) {
+			System.out.print(l + " ");
+		}
 	}
 }
