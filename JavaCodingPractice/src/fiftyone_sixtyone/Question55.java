e66ce6a9621039a5f5a97f2b963aa4b12f6d544a
Tarun Kumar Raj <rajtarunkumar007@gmail.com>
2023-11-30 12:28:45 +0530

    modified

--- JavaCodingPractice/src/fiftyone_sixtyone/Question55.java
+++ JavaCodingPractice/src/fiftyone_sixtyone/Question55.java
@@ -2,4 +2,18 @@ package fiftyone_sixtyone;
 
 public class Question55 {
 
+	public static String printSingleLine(int n) {
+		int i = 1;
+		StringBuilder str = new StringBuilder();
+
+		while (i <= n) {
+			str.append(i).append(" ");
+			i++;
+		}
+		return str.toString().trim();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(printSingleLine(5));
+	}
 }
