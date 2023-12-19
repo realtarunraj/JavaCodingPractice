1d340a0c0daa8edc0a741443bdf4916dfcd25d1a
Tarun Kumar Raj <rajtarunkumar007@gmail.com>
2023-12-19 18:41:57 +0530

    reverse the string

--- JavaCodingPractice/src/fiftyone_sixtyone/Question55.java
+++ JavaCodingPractice/src/fiftyone_sixtyone/Question55.java
@@ -1,19 +1,19 @@
 package fiftyone_sixtyone;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Question55 {
-
-	public static String printSingleLine(int n) {
-		int i = 1;
-		StringBuilder str = new StringBuilder();
-
-		while (i <= n) {
-			str.append(i).append(" ");
-			i++;
-		}
-		return str.toString().trim();
-	}
-
 	public static void main(String[] args) {
-		System.out.println(printSingleLine(5));
+		List<String> list = Arrays.asList("Green", "White", "Black", "Pink", "Orange", "Blue", "Champagne", "Indigo",
+				"Ivory");
+
+		list.sort(Comparator.comparingInt(String::length));
+		System.out.println("List in Ascending Order : " + list);
+
+		Collections.reverse(list);
+		System.out.println("List in Decending Order : " + list);
 	}
 }
--- JavaCodingPractice/src/fiftyone_sixtyone/Question57.java
+++ JavaCodingPractice/src/fiftyone_sixtyone/Question57.java
@@ -1,5 +1,16 @@
 package fiftyone_sixtyone;
 
 public class Question57 {
-
+	public static void main(String[] args) {
+		String str = "Reverse words in a given string";
+		String[] strArr = str.trim().split(" ");
+		
+		String reverse =  "";
+		
+		for(int i=strArr.length-1; i>=0; i--) {
+			reverse = reverse + " " + strArr[i];
+		}
+		
+		System.out.println(reverse.trim());
+	}
 }
