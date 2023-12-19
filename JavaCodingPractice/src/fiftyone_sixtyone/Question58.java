d845a113f53be02036a4ddf91f229ec25ff572a5
Tarun Kumar Raj <rajtarunkumar007@gmail.com>
2023-12-19 19:01:39 +0530

    max occurence of a character

--- JavaCodingPractice/src/fiftyone_sixtyone/Question58.java
+++ JavaCodingPractice/src/fiftyone_sixtyone/Question58.java
@@ -1,5 +1,34 @@
 package fiftyone_sixtyone;
 
-public class Question58 {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Question58 {
+	public static void main(String[] args) {
+		String str = "test string";
+		char[] charArray = str.toCharArray();
+
+		int max = 0;
+		char charMax = '\0';
+
+		Map<Character, Integer> map = new HashMap<>();
+		for (Character c : charArray) {
+			if (c.equals(' ')) {
+				continue;
+			} else {
+				if (map.containsKey(c)) {
+					map.replace(c, map.get(c) + 1);
+				} else {
+					map.put(c, 1);
+				}
+			}
+			
+			if(map.get(c) > max) {
+				max = map.get(c);
+				charMax = c;
+			}
+		}
+		
+		System.out.println("The character used maximum times is : " + charMax);
+	}
 }
